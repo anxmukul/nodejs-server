f28be8cd69d3f8dd255270683d737d7a38d204c4
Abhishek Sah <abhishek.sah@go-jek.com>
2021-01-21 20:07:21 +0530

    [Abhishek] Connect to local database

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const { Client } = require('pg')
 const express = require('express')
 const app = express()
 const port = 3000
+
+const client = new Client({
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'blog',
+  password: '',
+  port: 54320,
+})
 
 app.get('/', (req, res) => {
   res.sendFile('views/root.html', {root: __dirname })
@@ -11,5 +20,7 @@ app.get('/new', (req, res) => {
 })
 
 app.listen(port, () => {
+  client.connect()
+  console.log(`connected to database: successfully`)
   console.log(`Example app listening at http://localhost:${port}`)
 })
