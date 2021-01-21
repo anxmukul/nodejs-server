73dd6c726131497a0358f961cc23b5058e40fd39
Abhishek Sah <abhishek.sah@go-jek.com>
2021-01-21 20:41:32 +0530

    [Abhishek] Render blog page with dynamic values

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const { Client } = require("pg");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+
 app.use(bodyParser.urlencoded());
+app.set("view engine", "ejs");
+
 const port = 3000;
 
 const client = new Client({
@@ -21,17 +24,30 @@ app.get("/new", (req, res) => {
   res.sendFile("views/newBlog.html", { root: __dirname });
 });
 
+app.get("/blog/:title", (req, res) => {
+  const blogTitle = req.params.title;
+  const selectQuery = `select * from blogs where title='${blogTitle}'`;
+  client.query(selectQuery, (err, result) => {
+    if (err) {
+      res.send("error");
+    } else {
+      res.render("blog", {
+        title: result.rows[0].title,
+        content: result.rows[0].content,
+      });
+    }
+  });
+});
+
 app.post("/blog", (req, res) => {
   const title = req.body.title;
   const content = req.body.content;
   const insertQuery = `insert into blogs(title, content) VALUES ('${title}', '${content}')`;
-  console.log(insertQuery);
   client.query(insertQuery, (err, result) => {
     if (err) {
-      //res.send()
       console.log("failure", err);
     } else {
-      res.send("blog created");
+      res.redirect(`/blog/${title}`);
     }
   });
 });
